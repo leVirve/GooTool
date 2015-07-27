db6c92418441cf1b24833217289f002b87abaebb
leVirve <gae.m.project@gmail.com>
2015-07-27 14:30:49 +0800

    use class rebuild manager

--- google_drive_ex.py
+++ google_drive_ex.py
@@ -11,90 +11,87 @@ try:
 except ImportError:
     flags = None
 
-SCOPES = 'https://www.googleapis.com/auth/drive'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Drive download'
 
+class DriveMan():
 
-def get_credentials():
-    """Gets valid user credentials from storage.
+    SCOPES = 'https://www.googleapis.com/auth/drive'
+    CLIENT_SECRET_FILE = 'client_secret.json'
+    APPLICATION_NAME = 'Drive download'
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+    def __init__(self):
+        self.credentials = self._get_credentials()
+        self.service = self._new_service()
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    credential_dir = os.path.join('./', '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir, 'drive-credential.json')
+    def _get_credentials(self):
+        """Gets valid user credentials from storage.
 
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatability with Python 2.6
-            credentials = tools.run(flow, store)
-    return credentials
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
 
+        Returns:
+            Credentials, the obtained credential.
+        """
+        credential_dir = os.path.join('./', '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir, 'drive-credential.json')
 
-def main():
-    """Shows basic usage of the Google Drive API.
+        store = oauth2client.file.Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(
+                DriveMan.CLIENT_SECRET_FILE, DriveMan.SCOPES)
+            flow.user_agent = DriveMan.APPLICATION_NAME
+            if flags:
+                credentials = tools.run_flow(flow, store, flags)
+            else:  # Needed only for compatability with Python 2.6
+                credentials = tools.run(flow, store)
+        return credentials
 
-    Creates a Google Drive API service object and outputs the names and IDs.
-    """
-    credentials = get_credentials()
-    httplib = credentials.authorize(httplib2.Http())
-    service = discovery.build('drive', 'v2', http=httplib)
+    def _new_service(self):
+        """Creates a Google Drive API service object.
+        """
+        httplib = self.credentials.authorize(httplib2.Http())
+        return discovery.build('drive', 'v2', http=httplib)
 
-    item = print_file_metadata(service, 'FILE ID')
-    with open(item['title'], 'wb') as f:
-        f.write(download_file(service, item))
+    def get_metadata(self, file_id):
+        try:
+            return self.service.files().get(fileId=file_id).execute()
+        except errors.HttpError as error:
+            print('An error occurred: %s' % error)
 
+    def download(self, file_id):
+        """Download a file's content.
 
-def print_file_metadata(service, file_id):
-    """Print a file's metadata.
+        Args:
+        file_id: Drive File id.
 
-    Args:
-    service: Drive API service instance.
-    file_id: ID of the file to print metadata for.
-    """
-    try:
-        file = service.files().get(fileId=file_id).execute()
+        Returns:
+        Response data from Google Drive if successful, None otherwise.
+        """
+        drive_file = self.get_metadata(file_id)
+        download_url = drive_file.get('downloadUrl')
 
-        print('Title: %s' % file['title'])
-        return file
-    except errors.HttpError as error:
-        print('An error occurred: %s' % error)
-
-
-def download_file(service, drive_file):
-    """Download a file's content.
-
-    Args:
-    service: Drive API service instance.
-    drive_file: Drive File instance.
-
-    Returns:
-    File's content if successful, None otherwise.
-    """
-    download_url = drive_file.get('downloadUrl')
-    if download_url:
-        print('Go', download_url)
-        resp, content = service._http.request(download_url)
-        if resp.status == 200:
-            print('Status: %s' % resp)
-            return content
+        if download_url:
+            resp, content = self.service._http.request(download_url)
+            if resp.status == 200:
+                with open(drive_file['originalFilename'], 'wb') as f:
+                    f.write(content)
+                return resp
+            else:
+                print('An error occurred: %s' % resp)
+                return None
         else:
-            print('An error occurred: %s' % resp)
+            # The file doesn't have any content stored on Drive.
             return None
-    else:
-        # The file doesn't have any content stored on Drive.
-        return None
 
 if __name__ == '__main__':
-    main()
+    drive = DriveMan()
+
+    # get meta data by id
+    meta = drive.get_metadata('XXXXXXXXXXXXXXXXXXXXX')
+    print(meta)
+
+    # download file by id
+    resp = drive.download('XXXXXXXXXXXXXXXXXXXXX')
+    print(resp)

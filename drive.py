f4477bdf79fdbdea465e47fd87160006d6bb443c
leVirve <gae.m.project@gmail.com>
2015-07-27 16:05:06 +0800

    sub-service inherent core service

--- drive.py
+++ drive.py
@@ -1,9 +1,7 @@
-import httplib2
-import os
+from apiclient import errors
+from oauth2client import tools
 
-import oauth2client
-from apiclient import discovery, errors
-from oauth2client import client, tools
+import google_service
 
 try:
     import argparse
@@ -12,52 +10,17 @@ except ImportError:
     flags = None
 
 
-class DriveMan():
+class DriveMan(google_service.Gooooogle):
 
     SCOPES = 'https://www.googleapis.com/auth/drive'
     CLIENT_SECRET_FILE = 'client_secret.json'
+    CREDENTIALS_NAME = 'drive-credential.json'
     API_NAME = "drive"
     API_VERSION = 'v2'
     APPLICATION_NAME = 'Drive download'
 
     def __init__(self):
-        self.credentials = self._get_credentials()
-        self.service = self._new_service()
-
-    def _get_credentials(self):
-        """Gets valid user credentials from storage.
-
-        If nothing has been stored, or if the stored credentials are invalid,
-        the OAuth2 flow is completed to obtain the new credentials.
-
-        Returns:
-            Credentials, the obtained credential.
-        """
-        credential_dir = os.path.join('./', '.credentials')
-        if not os.path.exists(credential_dir):
-            os.makedirs(credential_dir)
-        credential_path = os.path.join(credential_dir, 'drive-credential.json')
-
-        store = oauth2client.file.Storage(credential_path)
-        credentials = store.get()
-        if not credentials or credentials.invalid:
-            flow = client.flow_from_clientsecrets(
-                DriveMan.CLIENT_SECRET_FILE, DriveMan.SCOPES)
-            flow.user_agent = DriveMan.APPLICATION_NAME
-            if flags:
-                credentials = tools.run_flow(flow, store, flags)
-            else:  # Needed only for compatability with Python 2.6
-                credentials = tools.run(flow, store)
-        return credentials
-
-    def _new_service(self):
-        """Creates a Google Drive API service object.
-        """
-        httplib = self.credentials.authorize(httplib2.Http())
-        return discovery.build(
-            DriveMan.API_NAME,
-            DriveMan.API_VERSION,
-            http=httplib)
+        super(DriveMan, self).__init__()
 
     def get_metadata(self, file_id):
         try:
@@ -65,30 +28,6 @@ class DriveMan():
         except errors.HttpError as error:
             print('An error occurred: %s' % error)
 
-    def download(self, file_id):
-        """Download a file's content.
-
-        Args:
-        file_id: Drive File id.
-
-        Returns:
-        Response data from Google Drive if successful, None otherwise.
-        """
-        drive_file = self.get_metadata(file_id)
-        download_url = drive_file.get('downloadUrl')
-
-        if download_url:
-            resp, content = self.service._http.request(download_url)
-            if resp.status == 200:
-                with open(drive_file['originalFilename'], 'wb') as f:
-                    f.write(content)
-                return resp
-            else:
-                print('An error occurred: %s' % resp)
-                return None
-        else:
-            # The file doesn't have any content stored on Drive.
-            return None
 
 if __name__ == '__main__':
     drive = DriveMan()
--- youtube.py
+++ youtube.py
@@ -1,9 +1,6 @@
-import httplib2
-import os
+from oauth2client import tools
 
-import oauth2client
-from apiclient import discovery
-from oauth2client import client, tools
+import google_service
 
 try:
     import argparse
@@ -12,83 +9,44 @@ except ImportError:
     flags = None
 
 
-class YoutubeMe():
+class YoutubeMe(google_service.Gooooogle):
 
     SCOPES = 'https://www.googleapis.com/auth/youtube.readonly'
-    CLIENT_SECRETS_FILE = 'client_secret.json'
+    CLIENT_SECRETS_FILE = 'client_secret_youtube.json'
+    CREDENTIALS_NAME = 'youtube-credential.json'
+    APPLICATION_NAME = 'Youtube got playlists'
     API_NAME = "youtube"
     API_VERSION = 'v3'
 
     def __init__(self):
-        self.credentials = self._get_credentials()
-        self.service = self._new_service()
-
-    def _get_credentials(self):
-        """Gets valid user credentials from storage.
-
-        If nothing has been stored, or if the stored credentials are invalid,
-        the OAuth2 flow is completed to obtain the new credentials.
-
-        Returns:
-            Credentials, the obtained credential.
-        """
-        credential_dir = os.path.join('./', '.credentials')
-        if not os.path.exists(credential_dir):
-            os.makedirs(credential_dir)
-        credential_path = os.path.join(credential_dir, 'youtube-credential.json')
-
-        store = oauth2client.file.Storage(credential_path)
-        credentials = store.get()
-        if not credentials or credentials.invalid:
-            flow = client.flow_from_clientsecrets(
-                YoutubeMe.CLIENT_SECRETS_FILE, YoutubeMe.SCOPES)
-            flow.user_agent = YoutubeMe.APPLICATION_NAME
-            if flags:
-                credentials = tools.run_flow(flow, store, flags)
-            else:  # Needed only for compatability with Python 2.6
-                credentials = tools.run(flow, store)
-        return credentials
-
-    def _new_service(self):
-        """Creates a Google Drive API service object.
-        """
-        httplib = self.credentials.authorize(httplib2.Http())
-        return discovery.build(
-            YoutubeMe.API_NAME,
-            YoutubeMe.API_VERSION,
-            http=httplib)
+        super(YoutubeMe, self).__init__()
 
     def get_playlists(self):
         """Retrieve at most 50 playlists at once
         """
         playlists_response = self.service.playlists().list(
-            mine=True,
-            part="snippet",
-            maxResults=50
+            mine=True, part="snippet", maxResults=50
         ).execute()
 
         f = open('youtube-lists.txt', 'w', encoding='utf8')
         for playlist in playlists_response["items"]:
-            uploads_list_id = playlist["id"]
-
-            f.write('========= 清單 %s ==========\n' % playlist['snippet']['title'])
+            f.write('===== 清單 %s =====\n' % playlist['snippet']['title'])
 
             playlistitems_list_request = self.service.playlistItems().list(
-                playlistId=uploads_list_id,
-                part="snippet",
-                maxResults=50
+                playlistId=playlist["id"], part="snippet", maxResults=50
             )
-
             while playlistitems_list_request:
                 playlistitems_list_response = playlistitems_list_request.execute()
-
-                # Print information about each video.
                 for playlist_item in playlistitems_list_response["items"]:
                     title = playlist_item["snippet"]["title"]
                     video_id = playlist_item["snippet"]["resourceId"]["videoId"]
                     f.write("%s (%s)\n" % (title, video_id))
 
-                playlistitems_list_request = self.service.playlistItems().list_next(playlistitems_list_request, playlistitems_list_response)
+                playlistitems_list_request = self.service.playlistItems(). \
+                    list_next(
+                        playlistitems_list_request,
+                        playlistitems_list_response
+                    )
 
 if __name__ == '__main__':
     youtube = YoutubeMe()
